5007af0c4186129365a0a3e874e30d8be6564b0a
Behruz <madaminovbehruz746@gmail.com>
2024-06-23 21:26:15 +0500

    added: module-events

---
+++ 04-dars-module-events/event.js
@@ -0,0 +1,25 @@
+const Events = require("events");
+
+// Logger classiga extends yani voris sifatida Eventsni olish
+// voris bolganidan song Events dagi barcha methodlar Loggerga ham otadi
+class Logger extends Events {
+  // Logger ichida function yaratish
+  log(a, b) {
+    // emit() agar log ishga tushsa emitdagi malumotni ko'rsatadi
+    // function ichida emit eventini qilish va nome berish "calculate" yana eventga funtion yozish yani "a + b"
+    this.emit("calculate", a + b);
+  }
+}
+
+// Logger classini logger saqlovchisiga exemplat qilib olish
+const logger = new Logger();
+
+// logger orqali on() methodini chaqirish
+// on() methodi ishituvchi uni ichga emit() nomi yani "calculate" va callback function malumotlarni ko'rsatish uchun
+logger.on("calculate", (data) => {
+  console.log(data);
+});
+
+// class ichidagi logni ishga tushirib beradi
+// ! hardoim class functioni class on dan kegin yozish kerak
+logger.log(1, 99);
